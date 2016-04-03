40e0076be97154c97645fb6441f848b908d72962
NativeForeigner <nativeforeignerwiki@gmail.com>
2016-04-02 22:13:34 -0700

    changing env

--- findRep.py
+++ findRep.py
@@ -19,12 +19,18 @@ if not(federalLegislators or stateLegislators):
     print "Non US Address, try again"
     quit()
 
+
 print "State Legislators:"
 for stateRep in stateLegislators:
     print "Rep. {} is a {} member of the {} house".format(stateRep['last_name'],stateRep['party'],stateRep['chamber'])
+
+
 print "Federal Legislators:"
+
 for federalRep in federalLegislators:
     if federalRep['chamber']=='senate':
         print "Senator {} is a {} member of the {} from {}".format(federalRep['last_name'],federalRep['state_rank'],federalRep['chamber'],federalRep['state_name'])
     if federalRep['chamber']=='house':
         print "Representative {} is a member of the {} from {}".format(federalRep['last_name'],federalRep['chamber'],federalRep['state_name'])
+
+

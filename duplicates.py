598081dac8b81af64992344bac3ae60659c00e5f
NativeForeigner <nativeforeignerwiki@gmail.com>
2016-04-10 20:37:02 -0700

    unicode issues fixed

--- duplicates.py
+++ duplicates.py
@@ -1,5 +1,6 @@
 import sunlight
 from collections import Counter
+from unidecode import unidecode
 
 legislators = sunlight.congress.all_legislators_in_office()
 
@@ -8,19 +9,18 @@ house = list()
 both = list()
 for legislator in legislators:
     if legislator['chamber'] == 'senate':
-        senate.append(legislator['last_name'])
+        senate.append(unidecode(legislator['last_name']))
     if legislator['chamber'] == 'house':
-        house.append(legislator['last_name'])
-    #both.append(legislator['last_name'])
+        house.append(unidecode(legislator['last_name']))
+    both.append(unidecode(legislator['last_name']))
 
-    both = set(senate).intersection(house)
 senate = [senator.encode('utf-8') for senator in senate]
 house = [rep.encode('utf-8') for rep in house]
-bothduplicates = [bothrep.encode('utf-8') for bothrep in both]
+both = [bothrep.encode('utf-8') for bothrep in both]
 
 senateduplicates = [k for k, v in Counter(senate).items() if v>1]
 houseduplicates = [l for l, m in Counter(house).items() if m>1]
-#bothduplicates = [x for x, p in Counter(both).items() if p>1]
+bothduplicates = [x for x, p in Counter(both).items() if p>1]
 
 print senateduplicates
 print houseduplicates

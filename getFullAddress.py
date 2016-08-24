4af6cc93bc8edb451f978d367593a3bfdd24905d
Brian Cox <bcox@ucsd.edu>
2016-08-24 02:23:53 -0400

    fix emerging bug, try to get updating to be more robust

--- getFullAddress.py
+++ getFullAddress.py
@@ -5,13 +5,15 @@ url ="https://www.govtrack.us/api/v2/role?current=true&limit=542&fields=person__
 response = urllib.urlopen(url)
 address = json.loads(response.read())
 addressDict = dict()
-for i in range(len(address['objects'])):
-    try:
-        addressDict[address['objects'][i]['person']['bioguideid']]=address['objects'][i]['extra']['address'].replace("HOB","House Office Building")
-    except KeyError:
-        addressDict[address['objects'][i]['person']['bioguideid']]="not currently known"
-    except:
-        print "Unexpected error"
 
+for i in range(len(address['objects'])):
+    if address['objects'][i]['extra']:
+        try:
+            addressDict[address['objects'][i]['person']['bioguideid']]=address['objects'][i]['extra']['address'].replace("HOB","House Office Building")
+        except KeyError:
+            addressDict[address['objects'][i]['person']['bioguideid']]="not currently known"
+	except:
+   	    print "Unexpected error"
+	    print addressDict[address['objects'][i]['person']['bioguideid']]
 with open('addressDict.json','w') as f:
     json.dump(addressDict,f)
